a66f15b30b89a704248e129dbea4aca35f08f419
Valerio Varvaglione <vvalerio94@hotmail.it>
2023-03-09 17:47:57 +0100

    seconda parte java

---
+++ java2.js
@@ -0,0 +1,84 @@
+// let v = prompt (`che voto hai preso?`);
+
+// if (v < 18) {
+//     alert (`Insufficiente`)
+
+// } else if (v >= 18 && v < 21) {
+    
+//     alert (`Sufficiente`)
+// }
+    
+//     alert (`Buono`)
+// }
+
+// else if (v >= 24 && v < 27) {
+    
+//     alert (`Distinto`)
+// }
+
+// else if (v >= 27 && v < 29) {
+    
+//     alert (`Ottimo`)
+// }
+
+
+// let num = prompt (`di che numero vuoi la tabellina?`);
+// for (tabellina = 1; tabellina <= 10; tabellina++){
+//     console.log(num * tabellina);
+
+
+// // }
+
+// let temperatura = prompt (`che temperatura c'è?`);
+
+// if (temperatura <20 && temperatura >=0){
+//     alert(`non ci sono più`);
+// } else if (temperatura >= 30){
+    
+//     alert(`lu mare, lu sole, lu ientu`);
+// }
+ 
+// else if (temperatura <30 && temperatura >=20){
+//     alert(`mi dia una peroni sudata`);
+// }
+
+// else if (temperatura < 0 && temperatura >= -10){
+//     alert(`non è tanto il freddo quanto l'umidità`);
+// }
+
+// else if (temperatura < -10){
+//     alert (`copriti... ancora ti raffreddi`);
+// }
+
+// let distributore = promt (`quale bevanda desideri¡`)
+// if (distributore == 1){
+//     alert(`è stata selezionata l'acqua`);
+// }
+// else if (distributore == 2){
+//     alert(`è stata selezionata coca cola`);
+// }
+// else if (distributore == 3){
+//     alert(`è stata selezionata la birra`);
+// } else while (distributore != 1 && distributore != 2 && distributore !=3) {
+//  prompt (`quale bevanda scegli`);
+// }
+
+
+
+
+
+
+
+
+
+
+// - ES 4: Scrivere un programma che simuli un distributore di bevande:
+//         - fare in modo che l’utente possa inserire il numero corrispondente alla bevanda, mediante l’istruzione prompt:
+//             - se inserisce 1, seleziona acqua e quindi stampare in console: “E’ stata selezionata l’acqua”
+//             - se inserisce 2, seleziona coca cola e quindi stampare in console: “E’ stata selezionata coca cola”
+//             - se inserisce 3, seleziona birra e quindi stampare in console: “E’ stata selezionata birra”
+//             - se inserisce qualcosa di diverso, il programma dovra’ riproporre la domanda di partenza
+
+
+
+
